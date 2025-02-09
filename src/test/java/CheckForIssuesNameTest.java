a69a3ef2c7e056d12a36158fb80cd2f4bf3f1ef0
krasnova.ilvera <ilvera.krasnova@yandex.ru>
2025-02-09 22:58:44 +0300

    added steps

--- src/test/java/CheckForIssuesNameTest.java
+++ src/test/java/CheckForIssuesNameTest.java
@@ -1,10 +1,13 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
 import static org.openqa.selenium.By.linkText;
 
 public class CheckForIssuesNameTest {
@@ -14,24 +17,49 @@ public class CheckForIssuesNameTest {
         Configuration.baseUrl = "https://github.com/";
         Configuration.browserSize = "1928x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true; //потом удалить
-
     }
 
+    private static final String Repository = "IlveraKrasnova-QA/AllureReportsHomeWork";
+    private static final String Issues = "simple issue";
+
     @Test
-    void checkForIssuesNameTest() {
+    public void checkForIssuesNameSelenideTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
         open("");
         $("[class=search-input]").click();
         $("[id=\"query-builder-test\"]").setValue("IlveraKrasnova-QA/AllureReportsHomeWork").submit();
         $(linkText("IlveraKrasnova-QA/AllureReportsHomeWork")).click();
         $("#issues-tab").click();
         $("[class=Title-module__container--l9xi7]").shouldHave(text("simple issue"));
+    }
 
+    @Test
+    public void checkForIssuesNameWithStepsTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
+        step("Открываем главную страницу", () -> {
+            open("");
+                });
 
+        step("Ищем репозиторий" + Repository, () -> {
+            $("[class=search-input]").click();
+            $("[id=\"query-builder-test\"]").setValue(Repository).submit();
+        });
 
+        step("Кликаем на найденный реппозиторий" + Repository, () -> {
+            $(linkText(Repository)).click();
+        });
 
+        step("Открываем раздел Issues", () -> {
+            $("#issues-tab").click();
+        });
 
+        step("Находим нужный Issue" + Issues, () -> {
+            $("[class=Title-module__container--l9xi7]").shouldHave(text(Issues));
+        });
 
     }
+
+
 }
